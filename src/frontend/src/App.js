b2eaea622540fd773e6278040789acd07498deaa
eyasuh <eyha12@gmail.com>
2022-03-09 21:25:04 -0500

    added antD Drawer Form

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,19 +1,30 @@
-import { useState, useEffect } from 'react'
-import { getAllStudents } from "./client";
-import {Layout, Menu, Breadcrumb, Table, Spin, Empty} from 'antd';
+import {useState, useEffect} from 'react'
+import {getAllStudents} from "./client";
+import {
+    Layout,
+    Menu,
+    Breadcrumb,
+    Table,
+    Spin,
+    Empty,
+    Button,
+} from 'antd';
+
 import {
     DesktopOutlined,
     PieChartOutlined,
     FileOutlined,
     TeamOutlined,
     UserOutlined,
-    LoadingOutlined
+    LoadingOutlined,
+    PlusOutlined
 } from '@ant-design/icons';
+import StudentDrawerForm from "./StudentDrawerForm";
 
 import './App.css';
 
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+const {Header, Content, Footer, Sider} = Layout;
+const {SubMenu} = Menu;
 
 const columns = [
     {
@@ -37,12 +48,13 @@ const columns = [
         key: 'gender',
     },
 ];
-const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+const antIcon = <LoadingOutlined style={{fontSize: 24}} spin/>;
 
 function App() {
     const [students, setStudents] = useState([]);
     const [collapsed, setCollapsed] = useState(false);
     const [fetching, setFetching] = useState(true);
+    const [showDrawer, setShowDrawer] = useState(false);
 
     const fetchStudents = () =>
         getAllStudents()
@@ -63,58 +75,71 @@ function App() {
             return <Spin indicator={antIcon}/>
         }
         if (students.length <= 0) {
-            return <Empty />;
+            return <Empty/>;
         }
-        return <Table
-            dataSource={students}
-            columns={columns}
-            title={() => 'Students'}
-            pagination={{ pageSize: 50 }} scroll={{ y: 240 }}
-            footer={() => 'More data coming'}
-            rowKey={students => students.id}
-        />;
+        return <>
+            <StudentDrawerForm
+                showDrawer={showDrawer}
+                setShowDrawer={setShowDrawer}
+            />
+            <Table
+                dataSource={students}
+                columns={columns}
+                bordered
+                title={() =>
+                    <Button
+                        onClick={() => setShowDrawer(!showDrawer)}
+                        type="primary" shape="round" icon={<PlusOutlined/>} size="small">
+                        Add New Student
+                    </Button>
+                }
+                pagination={{pageSize: 50}}
+                scroll={{y: 500}}
+                rowKey={student => student.id}
+            />
+        </>
+
     }
 
-    return <Layout style={{ minHeight: '100vh' }}>
+    return <Layout style={{minHeight: '100vh'}}>
         <Sider collapsible collapsed={collapsed}
                onCollapse={setCollapsed}>
-            <div className="logo" />
+            <div className="logo"/>
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<PieChartOutlined />}>
+                <Menu.Item key="1" icon={<PieChartOutlined/>}>
                     Option 1
                 </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined />}>
+                <Menu.Item key="2" icon={<DesktopOutlined/>}>
                     Option 2
                 </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
                     <Menu.Item key="3">Tom</Menu.Item>
                     <Menu.Item key="4">Bill</Menu.Item>
                     <Menu.Item key="5">Alex</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
                     <Menu.Item key="6">Team 1</Menu.Item>
                     <Menu.Item key="8">Team 2</Menu.Item>
                 </SubMenu>
-                <Menu.Item key="9" icon={<FileOutlined />}>
+                <Menu.Item key="9" icon={<FileOutlined/>}>
                     Files
                 </Menu.Item>
             </Menu>
         </Sider>
         <Layout className="site-layout">
-            <Header className="site-layout-background" style={{ padding: 0 }} />
-            <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
+            <Header className="site-layout-background" style={{padding: 0}}/>
+            <Content style={{margin: '0 16px'}}>
+                <Breadcrumb style={{margin: '16px 0'}}>
                     <Breadcrumb.Item>User</Breadcrumb.Item>
                     <Breadcrumb.Item>Bill</Breadcrumb.Item>
                 </Breadcrumb>
-                <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
                     {renderStudents()}
                 </div>
             </Content>
-            <Footer style={{ textAlign: 'center' }}>By Eyasu Haile</Footer>
+            <Footer style={{textAlign: 'center'}}>By Amigoscode</Footer>
         </Layout>
     </Layout>
 }
 
 export default App;
-
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,12 +1,11 @@
 package com.example.demo.student;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Queue;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
@@ -17,5 +16,10 @@ public class StudentController {
 	@GetMapping
 	public List<Student> getAllStudents() {
 		return studentService.getAllStudents();
+	}
+
+	@PostMapping
+	public void addStudent(@RequestBody Student student) {
+		studentService.addStudent(student);
 	}
 }
